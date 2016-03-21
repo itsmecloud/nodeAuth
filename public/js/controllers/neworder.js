792bb67d63b07adfa00c6726eb4fcd1477ffd613
RamKosal <ramesh03k@gmail.com>
2016-03-21 22:46:31 +0530

    functionality

--- public/js/controllers/neworder.js
+++ public/js/controllers/neworder.js
@@ -2,19 +2,12 @@ angular.module('orderController', [])
 
 	// inject the Todo service factory into our controller
 	.controller('neworderController', ['$scope','$http','Pricebook', function($scope, $http, Pricebook) {
-		$scope.pricebook = null;
+		$scope.pricebook = {};
 		$scope.pricebooks = [];
 		$scope.showList = false;
+		$scope.selected = true;
 		Pricebook.get()
 		.success(function(data) {
 			$scope.pricebooks = data;
 		});
-		$scope.showPriceBook = function(){
-			if($scope.pricebook != '' || $scope.pricebook != ' '){
-				$('#lookup-menu').show();
-			}else{
-				$('#lookup-menu').hide();
-			}
-			
-		}
 	}]);
