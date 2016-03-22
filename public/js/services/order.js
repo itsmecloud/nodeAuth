957d462bf6cc693ffe8571ffdd629a05653b4827
Ramesh Kosalairaman <ramesh03k@gmail.com>
2016-03-22 19:58:10 +0530

    Update order.js

--- public/js/services/order.js
+++ public/js/services/order.js
@@ -7,8 +7,20 @@ angular.module('orderService', [])
 			get : function() {
 				return $http.get('/api/order');
 			}
-			post : function() {
-				return $http.post('/api/order');
+			post : function(data) {
+				var input = $.param({
+            					json: JSON.stringify(data)
+				});
+				return $http.post('/api/order',input);
+			}
+		}
+	}])..factory('OrderItem', ['$http',function($http) {
+		return {
+			post : function(data) {
+				var input = $.param({
+            					json: JSON.stringify(data)
+				});
+				return $http.post('/api/orderitem',input);
 			}
 		}
 	}]).factory('Contracts', ['$http',function($http) {
